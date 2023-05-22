c274e73590c103818a2fb8ebedcfd16bd83ce086
Fome-a <ofomearubs@gmail.com>
2023-05-22 19:55:36 +0100

    tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
-""
+"""Building a simple cache"""
 import uuid
 import redis
-<<<<<<< HEAD
-from typing import Union
-
-class Cache:
-    """Cache class"""
-    def __init__(self)-> None:
-        """Cahce Instance"""
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, float, int]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return(key, data)
-=======
 from functools import wraps
 import sys
 from typing import Callable, Optional, Union
@@ -31,6 +16,7 @@ def count_calls(method: Callable) -> Callable:
         """decorator method"""
         self._redis.incr(key)
         return method(self, *args, **kwargs)
+
     return counter
 
 
@@ -45,6 +31,7 @@ def call_history(method: Callable) -> Callable:
         output = method(self, *args, **kwargs)
         self._redis.rpush(out_list_key, str(output))
         return output
+
     return history_wrapper
 
 
@@ -60,12 +47,14 @@ def replay(method: Callable) -> None:
     output_list = server.lrange(outputs, 0, -1)
     zipped = list(zip(input_list, output_list))
     for k, v in zipped:
-        attr, result = k.decode("utf-8"), k.decode("utf-8")
+        attr, result = k.decode("utf-8"), v.decode("utf-8")
         print(f"{key}(*{attr}) -> {result}")
+
 
 class Cache:
     """Cache class"""
-    def __init__(self,redis)-> None:
+
+    def __init__(self, redis) -> None:
         """instantiating the cache class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -73,19 +62,19 @@ class Cache:
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Takes a data argument and returns a string """ 
+        """Takes a data argument and returns a string"""
         key = str(uuid.uuid4())
         self._redis.set(str(key), data)
         return str(key)
-    def get(self, key: str, fn: Optional[Callable] = None) ->\
-                Union[str, bytes, int, float]:
-            """retieves value from server, convert it to desired format"""
-            return fn(self._redis.get(key)) if fn else self._redis.get(key)
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float]:
+        """retrieves value from server, convert it to desired format"""
+        return fn(self._redis.get(key)) if fn else self._redis.get(key)
 
     def get_int(self, data_bytes: bytes) -> int:
         """convert data bytes from server back to int"""
         return int.from_bytes(data_bytes, sys.byteorder)
->>>>>>> 6cee8a52d9f5f4a16f76cb5ce8b0ab54ebede642
 
     def get_str(self, data_bytes: bytes) -> str:
         """convert data bytes from server back into str"""
