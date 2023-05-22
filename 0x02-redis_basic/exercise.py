e047fffd2d35c081e20189d35ef7291730fc806c
Fome-a <ofomearubs@gmail.com>
2023-05-22 18:14:56 +0000

    tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,14 +2,17 @@
 ""
 import uuid
 import redis
+from typing import Union
 
 class Cache:
-    def __init__(self,redis)-> None:
+    """Cache class"""
+    def __init__(self)-> None:
+        """Cahce Instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, float, int]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
-        return(key)
+        return(key, data)
 
